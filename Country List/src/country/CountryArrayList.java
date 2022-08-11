00593276a3bc205707f422c223d43b7fbffb06ef
diyarose775 <diyarose1988@gmail.com>
2022-08-11 17:07:34 -0400

    Second commit

---
+++ Country List/src/country/CountryArrayList.java
@@ -0,0 +1,32 @@
+package country;
+
+public class CountryArrayList {
+
+	public static void main(String[] args) {
+	
+		String country[] = new String[10];
+		 
+		country [0] = "Bangladesh ";
+		country [1] = "Canada ";
+		country [2] = "Switzerland ";
+		country [3] = "Singapur ";
+		country [4] = "Denmark ";
+		country [5] = "United States ";
+		country [6] = "India ";
+		country [7] = "Ireland ";
+		country [8] = "Germany ";
+		country [9] = "Italy ";
+		
+		
+		
+		for (int i = 0; i < country.length; i++) {
+			
+			System.out.println(country[i]);
+			
+			
+		}
+		
+		
+	}
+
+}
